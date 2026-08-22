8d9f48e60f0a8743e57192b38c3acb8d894a1c75
AmadouDiallo4 <51537445+AmadouDiallo4@users.noreply.github.com>
2026-08-22 23:00:47 +0200

    Change server listen address to '0.0.0.0'

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const prisma = require('./config/prisma');
 const startServer = async () => {
   try {
     await prisma.$connect();
-    app.listen(env.PORT, () => {
+
+    app.listen(env.PORT, '0.0.0.0', () => {
       console.log(`YASS backend running on port ${env.PORT}`);
     });
   } catch (error) {
